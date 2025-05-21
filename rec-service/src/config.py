9207f25aee83bc3c9ca1734027b43021a685aae1
ElfatihZiad <ziad.elf@gmail.com>
2025-05-21 17:31:20 -0500

    modified TOPIC_INTERVAL from 10 to 5

--- rec-service/src/config.py
+++ rec-service/src/config.py
@@ -6,5 +6,5 @@ POST_CREATED_TOPIC = 'post-created'
 POST_LIKED_TOPIC = 'post-liked'
 REC_GENERATED_TOPIC = 'rec-generated'
 RECOMMENDATION_INTERVAL_SECONDS = 60
-TOPIC_MODEL_UPDATE_INTERVAL_POSTS = 10 # Re-train topic model after this many new posts
+TOPIC_MODEL_UPDATE_INTERVAL_POSTS = 5 # Re-train topic model after this many new posts
 NUM_TOPICS = 5 # Number of topics for K-Means clustering
